303d4521aff86d2ddc31e32678bf808ff9fdcebd
John Filippone <johnpfilippone@yahoo.com>
2020-04-24 23:45:32 -0400

    Adds left side menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import LeftSideMenu from './components/LeftSideMenu';
 
 function App() {
   return (
     <div className="App">
+      <LeftSideMenu />
     </div>
   );
 }
---
+++ src/components/LeftSideMenu.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LeftSideMenu.css';
+
+function LeftSideMenu() {
+  return (
+    <div className="menu-container">
+    </div>
+  );
+}
+
+export default LeftSideMenu;
+
