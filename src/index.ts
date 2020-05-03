4d48a1f53dfaca329e6e3608e377a8be10ed7591
John Filippone <johnpfilippone@yahoo.com>
2020-05-03 12:35:16 -0400

    Adds morgan request logging

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,13 @@ const logger = getLogger();
 import loadConfig from './config';
 const config = loadConfig();
 
-// Graphql
-import express from 'express';
-import { ApolloServer, gql } from 'apollo-server-express';
-
 // Google Sheets API interface
 import SheetsOperatorFactory from './sheets/sheets-operator-factory';
 const sheetsOperatorFactory = new SheetsOperatorFactory(logger, config.sheets);
 const sheetsOperator = sheetsOperatorFactory.create();
 
+// Graphql
+import { ApolloServer, gql } from 'apollo-server-express';
 const typeDefs = gql`
     type Query {
         connecting: [[String!]!]
@@ -26,21 +24,27 @@ const typeDefs = gql`
 `;
 const resolvers = {
     Query: {
-        connecting: async () => {return await sheetsOperator.getSheet('1ucWB8jjQIYJa_K4K0NsDA9owCeWYs1buClTVvE2JqJw', 'Connecting Volume');},
-        weight: async () => {return await sheetsOperator.getSheet('1DRXq0Uo_eVzgnT4bwo202XAU9YWltCa_8W26jhEaaxQ', 'Metrics');},
-        //connecting: () => {return getConnectingData(auth);},
-        //weight: () => {return getBodyWeight(auth);},
+        connecting: async () => {
+            return await sheetsOperator.getSheet('1ucWB8jjQIYJa_K4K0NsDA9owCeWYs1buClTVvE2JqJw', 'Connecting Volume');
+        },
+        weight: async () => {
+            return await sheetsOperator.getSheet('1DRXq0Uo_eVzgnT4bwo202XAU9YWltCa_8W26jhEaaxQ', 'Metrics');
+        },
         tables: () => [
             ['Body Weight', '1DRXq0Uo_eVzgnT4bwo202XAU9YWltCa_8W26jhEaaxQ', 'Weight'],
             ['Body Measurments', '1DRXq0Uo_eVzgnT4bwo202XAU9YWltCa_8W26jhEaaxQ', 'Girth']
         ]
     },
 };
-const server = new ApolloServer({ typeDefs, resolvers });
-const app = express();
-server.applyMiddleware({ app });
+const graphQLServer = new ApolloServer({ typeDefs, resolvers });
 
+// Web Server
+import express from 'express';
+import morgan from 'morgan';
+const app = express();
+app.use(morgan('combined'));
+graphQLServer.applyMiddleware({ app });
 app.listen({ port: 4000 }, () => {
-    logger.info('Now browse to http://localhost:4000' + server.graphqlPath)
+    logger.info('Now browse to http://localhost:4000' + graphQLServer.graphqlPath)
     sheetsOperator.connect();
 });
