bb6ef1e917f4ccb319b3144022ca9f0fe5860172
John Filippone <johnpfilippone@yahoo.com>
2020-05-16 18:57:42 -0400

    Adds draggable demo to the app

--- react/src/App.js
+++ react/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { DndProvider } from 'react-dnd'
+import Backend from 'react-dnd-html5-backend'
 import Dashboard from './components/Dashboard';
+import Example from './components/example'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+        <Dashboard />
+        <DndProvider backend={Backend}>
+            <Example />
+        </DndProvider>
     </div>
   );
 }
