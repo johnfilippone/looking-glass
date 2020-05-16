7956b44c9a94e64375be126c260cf07c209b0ba4
John Filippone <johnpfilippone@yahoo.com>
2020-05-16 16:38:26 -0400

    Fixes context menu

--- react/src/components/Dashboard.tsx
+++ react/src/components/Dashboard.tsx
@@ -14,17 +14,28 @@ class Dashboard extends React.Component<any, any> {
         this.state = {
             contextMenuActive: false,
             contextMenuClickPosition: { xPosition: 0, yPosition: 0 },
+            studySheet: [],
+            exerciseSheet: [],
+            practiceSheet: [],
+            studyDates: [],
+            exerciseDates: [],
+            practiceDates: [],
             weightData: [],
             connectingData: [],
             studyData: [],
             practiceData: [],
             exerciseData: [],
-            exerciseWeekData: [],
-            practiceWeekData: [],
+            weightChartInput: {},
+            connectingChartInput: {},
+            exercisePieInput: {},
+            practicePieInput: {},
             eventsData: []
         };
 
-        this.toggleContextMenu = this.toggleContextMenu.bind(this);
+        this.toggleContextMenuOn = this.toggleContextMenuOn.bind(this);
+        this.toggleContextMenuOff = this.toggleContextMenuOff.bind(this);
+        this.clickListener = this.clickListener.bind(this);
+        this.clickInsideElement = this.clickInsideElement.bind(this);
         this.getClickPosition = this.getClickPosition.bind(this);
         this.getSheet = this.getSheet.bind(this);
         this.buildLineChartInput = this.buildLineChartInput.bind(this);
@@ -43,14 +54,54 @@ class Dashboard extends React.Component<any, any> {
             this.getSheet('1OArbMSXtJAsGGWvl_1V7u2lwUncutERJ-TArYZmmExc', 'Events')
         ];
         Promise.all(promises).then((sheets) => {
+            const weightSheet = sheets[0].sheets;
+            const weightDates = weightSheet.slice(3).filter((data: any) => { return data[1]; }).map((data: any) => { return data[0]; });
+            //const weightLine = {label: 'Body Weight (lbs)', data: weightSheet.slice(3).filter((data: any) => { return data[1]; }).map((data: any) => { return parseFloat(data[1]); })};
+            const weightAvgLine = {label: 'Trend (lbs)', data: weightSheet.slice(3).filter((data: any) => { return data[2]; }).map((data: any) => { return parseFloat(data[2]); })};
+            const weightChartInput = this.buildLineChartInput(weightDates, [weightAvgLine]);
+
+            const connectingData = sheets[1].sheets;
+            const connectingDates = connectingData.slice(1).filter((data: any) => { return data[7] && data[8]; }).map((data: any) => { return data[0]; });
+            const maxLine = {label: 'Max dbs', data: connectingData.slice(1).filter((data: any) => { return data[7] && data[8]; }).map((data: any) => { return parseFloat(data[8]); })};
+            const avgLine = {label: 'Avg dbs', data: connectingData.slice(1).filter((data: any) => { return data[7] && data[8]; }).map((data: any) => { return parseFloat(data[7]); })};
+            const connectingChartInput = this.buildLineChartInput(connectingDates, [maxLine, avgLine]);
+
+            const studySheet = sheets[2].sheets;
+            const studyDates = studySheet.map((entry: any) => { return entry[0]; });
+            const practiceSheet = sheets[3].sheets;
+            const practiceDates = practiceSheet.map((entry: any) => { return entry[0]; });
+            const exerciseSheet = sheets[4].sheets;
+            const exerciseDates = exerciseSheet.map((entry: any) => { return entry[0]; });
+
+            const exerciseWeekSheet = sheets[5].sheets;
+            const exerciseWeekData = exerciseWeekSheet.slice(1, exerciseWeekSheet.length-1);
+            const exercisePieLabels = exerciseWeekData.map((entry: any) => { return entry[0]; });
+            const exercisePieData = exerciseWeekData.map((entry: any) => {
+                const components = entry[1].split(':');
+                return (+components[0]) * 60 * 60 + (+components[1]) * 60 + (+components[2]);
+            });
+            const exercisePieInput = this.buildPieChartInput(exercisePieLabels, exercisePieData);
+
+            const practiceWeekSheet = sheets[6].sheets;
+            const practiceWeekData = practiceWeekSheet.slice(1, practiceWeekSheet.length-1);
+            const practicePieLabels = practiceWeekData.map((entry: any) => { return entry[0]; });
+            const practicePieData = practiceWeekData.map((entry: any) => {
+                const components = entry[1].split(':');
+                return (+components[0]) * 60 * 60 + (+components[1]) * 60 + (+components[2]);
+            });
+            const practicePieInput = this.buildPieChartInput(practicePieLabels, practicePieData);
+
             this.setState({
-                weightData: sheets[0].sheets,
-                connectingData: sheets[1].sheets,
-                studyData: sheets[2].sheets,
-                practiceData: sheets[3].sheets,
-                exerciseData: sheets[4].sheets,
-                exerciseWeekData: sheets[5].sheets,
-                practiceWeekData: sheets[6].sheets,
+                studySheet,
+                exerciseSheet,
+                practiceSheet,
+                studyDates,
+                exerciseDates,
+                practiceDates,
+                weightChartInput,
+                connectingChartInput,
+                exercisePieInput,
+                practicePieInput,
                 eventsData: sheets[7].sheets
             });
         });
@@ -60,13 +111,46 @@ class Dashboard extends React.Component<any, any> {
         return request('http://localhost:4000/graphql', `{ sheets(spreadsheetId:"${spreadsheetId}", range:"${range}") }`);
     }
 
-    toggleContextMenu(e: any) {
+    toggleContextMenuOn(e: any) {
         e.preventDefault();
         this.setState({
-            contextMenuActive: !this.state.contextMenuActive,
+            contextMenuActive: true,
             contextMenuClickPosition: this.getClickPosition(e)
         });
     }
+
+    toggleContextMenuOff(e: any) {
+        e.preventDefault();
+        this.setState({
+            contextMenuActive: false
+        });
+    }
+
+    clickListener(e: any) {
+        if (this.state.contextMenuActive) {
+            if (this.clickInsideElement(e, 'ContextMenu')) {
+                // handle menu links
+            } else {
+                e.preventDefault();
+                this.toggleContextMenuOff(e);
+            }
+        }
+    }
+
+    clickInsideElement(e: any, className: any) {
+        var el = e.srcElement || e.target;
+        if ( el.classList.contains(className) ) {
+            return el;
+        } else {
+            while (el = el.parentNode) {
+                if ( el.classList && el.classList.contains(className) ) {
+                    return el;
+                }
+            }
+        }
+        return false;
+    }
+
 
     getClickPosition(e: any) {
         let posx = 0;
@@ -126,80 +210,42 @@ class Dashboard extends React.Component<any, any> {
     }
 
     render() {
-        const weightSheet = this.state.weightData;
-        const weightDates = weightSheet.slice(3).filter((data: any) => { return data[1]; }).map((data: any) => { return data[0]; });
-        const weightLine = {label: 'Body Weight (lbs)', data: weightSheet.slice(3).filter((data: any) => { return data[1]; }).map((data: any) => { return parseFloat(data[1]); })};
-        const weightAvgLine = {label: 'Trend (lbs)', data: weightSheet.slice(3).filter((data: any) => { return data[2]; }).map((data: any) => { return parseFloat(data[2]); })};
-        const weightChartInput = this.buildLineChartInput(weightDates, [weightAvgLine]);
-
-        const connectingData = this.state.connectingData;
-        const connectingDates = connectingData.slice(1).filter((data: any) => { return data[7] && data[8]; }).map((data: any) => { return data[0]; });
-        const maxLine = {label: 'Max dbs', data: connectingData.slice(1).filter((data: any) => { return data[7] && data[8]; }).map((data: any) => { return parseFloat(data[8]); })};
-        const avgLine = {label: 'Avg dbs', data: connectingData.slice(1).filter((data: any) => { return data[7] && data[8]; }).map((data: any) => { return parseFloat(data[7]); })};
-        const connectingChartInput = this.buildLineChartInput(connectingDates, [maxLine, avgLine]);
-
-        const studySheet = this.state.studyData;
-        const studyDates = studySheet.map((entry: any) => { return entry[0]; });
-        const exerciseSheet = this.state.exerciseData;
-        const exerciseDates = exerciseSheet.map((entry: any) => { return entry[0]; });
-        const practiceSheet = this.state.practiceData;
-        const practiceDates = practiceSheet.map((entry: any) => { return entry[0]; });
-
-        const exerciseWeekSheet = this.state.exerciseWeekData;
-        const exerciseWeekData = exerciseWeekSheet.slice(1, exerciseWeekSheet.length-1);
-        const exercisePieLabels = exerciseWeekData.map((entry: any) => { return entry[0]; });
-        const exercisePieData = exerciseWeekData.map((entry: any) => {
-            const components = entry[1].split(':');
-            return (+components[0]) * 60 * 60 + (+components[1]) * 60 + (+components[2]);
-        });
-        const exercisePieInput = this.buildPieChartInput(exercisePieLabels, exercisePieData);
-
-        const practiceWeekSheet = this.state.practiceWeekData;
-        const practiceWeekData = practiceWeekSheet.slice(1, practiceWeekSheet.length-1);
-        const practicePieLabels = practiceWeekData.map((entry: any) => { return entry[0]; });
-        const practicePieData = practiceWeekData.map((entry: any) => {
-            const components = entry[1].split(':');
-            return (+components[0]) * 60 * 60 + (+components[1]) * 60 + (+components[2]);
-        });
-        const practicePieInput = this.buildPieChartInput(practicePieLabels, practicePieData);
-
-        const eventsData = this.state.eventsData;
         const now = new Date();
         const tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate()+1);
         const firstDayOfWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate()-now.getDay());
         const firstDayOfLastWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate()-now.getDay()-7);
 
         return (
-            <div className='Dashboard' onContextMenu={this.toggleContextMenu}>
+            <div className='Dashboard' onClick={this.clickListener} onContextMenu={this.toggleContextMenuOn}>
                 <div style={{gridColumn: '1 / 2', gridRow: '1 / 5'}}>
-                    <Streak title='Study Streak' dates={studyDates} lookback={30} />
-                    <Streak title='KTVA Practice Streak' dates={practiceDates} lookback={30} />
-                    <Streak title='Exercise Streak' dates={exerciseDates} lookback={30} />
+                    <Streak title='Study Streak' dates={this.state.studyDates} lookback={30} />
+                    <Streak title='KTVA Practice Streak' dates={this.state.practiceDates} lookback={30} />
+                    <Streak title='Exercise Streak' dates={this.state.exerciseDates} lookback={30} />
                     <Countdown title='Days until TX' date={Date.parse('01 Aug 2020 00:00:00 GMT')} />
                     <Countdown title='Days to End of Diet' date={Date.parse('21 June 2020 00:00:00 GMT')} />
                 </div>
                 <div className='flex-row' style={{gridColumn: '2 / 4', gridRow: '1 / 2'}}>
-                    <DailyProgress title='Daily Study' width={300} height={10} goal={7.14} unit='%' data={studySheet} parameter='SUM of % Completed'/>
-                    <DailyProgress title='Daily Exercise' width={300} height={10} goal={200} unit='Seconds' data={exerciseSheet} parameter='SUM of Time Under Tension'/>
-                    <DailyProgress title='Daily KTVA Practice' width={300} height={10} goal={2700} unit='Seconds' data={practiceSheet} parameter='SUM of Duration'/>
+                    <DailyProgress title='Daily Study' width={300} height={10} goal={7.14} unit='%' data={this.state.studySheet} parameter='SUM of % Completed'/>
+                    <DailyProgress title='Daily Exercise' width={300} height={10} goal={200} unit='Seconds' data={this.state.exerciseSheet} parameter='SUM of Time Under Tension'/>
+                    <DailyProgress title='Daily KTVA Practice' width={300} height={10} goal={2700} unit='Seconds' data={this.state.practiceSheet} parameter='SUM of Duration'/>
                 </div>
                 <div style={{gridColumn: '2 / 3', gridRow: '2 / 3'}}>
-                    <Pie data={exercisePieInput} />
+                    <Pie data={this.state.exercisePieInput} />
                 </div>
                 <div style={{gridColumn: '3 / 4', gridRow: '2 / 3'}}>
-                    <Pie data={practicePieInput} />
+                    <Pie data={this.state.practicePieInput} />
                 </div>
                 <div style={{gridColumn: '2 / 3', gridRow: '3 / 4'}}>
-                    <Line data={weightChartInput} />
+                    <Line data={this.state.weightChartInput} />
                 </div>
                 <div style={{gridColumn: '3 / 4', gridRow: '3 / 4'}}>
-                    <Line data={connectingChartInput} />
+                    <Line data={this.state.connectingChartInput} />
                 </div>
                 <div style={{gridColumn: '2 / 3', gridRow: '4 / 5'}}>
-                    <EventList title='Last Week' data={eventsData} significance={2} startDate={firstDayOfLastWeek} endDate={firstDayOfWeek} />
+                    <EventList title='Last Week' data={this.state.eventsData} significance={2} startDate={firstDayOfLastWeek} endDate={firstDayOfWeek} />
                 </div>
                 <div style={{gridColumn: '3 / 4', gridRow: '4 / 5'}}>
-                    <EventList title='This Week' data={eventsData} significance={2} startDate={firstDayOfWeek} endDate={tomorrow} />
+                    <EventList title='This Week' data={this.state.eventsData} significance={2} startDate={firstDayOfWeek} endDate={tomorrow} />
                 </div>
                 <ContextMenu active={this.state.contextMenuActive} clickPosition={this.state.contextMenuClickPosition} />
             </div>
